5137529029161583d183b8e71604a21aa780dbcb
vegabook <thomas@scendance.fr>
2024-11-29 19:32:55 +0000

    pong

--- gBLP/server_gblp.py
+++ gBLP/server_gblp.py
@@ -242,6 +242,7 @@ class Bbg(BbgServicer):
         timestamp.GetCurrentTime()
         message = request.message + " Pong"
         pong = Pong(timestamp=timestamp, message=message)
+        logger.info(f"Sending pong {pong} to {clientid}")
         return pong
 
 # ----------------- mains ----------------------
