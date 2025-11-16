00ee318e53b3b0a0e51dbc7ad67a11a751a31039
vegabook <thomas@scendance.fr>
2025-11-16 16:32:32 +0000

    final unpackaged commit

--- protos/make_protos.sh
+++ protos/make_protos.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 python -m grpc_tools.protoc -I. --python_out=../gBLP --grpc_python_out=../gBLP bloomberg.proto
-sed -i 's/^import bloomberg_pb2 as/import gBLP.bloomberg_pb2 as/' ../gBLP/bloomberg_pb2_grpc.py
 
