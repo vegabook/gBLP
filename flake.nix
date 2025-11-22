dffa11b5fe5baa16574e8f5ad437db2e06332704
vegabook <thomas@scendance.fr>
2025-11-22 14:34:15 +0000

    better flake

--- flake.nix
+++ flake.nix
@@ -1,80 +1,56 @@
 {
-  description = "Python 3.11 development environment";
+  description = "Develop Python on Nix with uv";
 
-  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  # Flake outputs
-  outputs = { self, nixpkgs }:
+  outputs =
+    { nixpkgs, ... }:
     let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
     in
     {
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = with pkgs; [
-            python312
-            python312Packages.grpcio
-            python312Packages.grpcio-tools
-            #python312Packages.ipython
-            #python312Packages.certifi
-            #python312Packages.cryptography
-            #python312Packages.rich
-            #python312Packages.numpy
-            #python312Packages.pandas
-            #python312Packages.polars
-            #python312Packages.psutil
-            #python312Packages.pytest
-            #python312Packages.poetry-core
-            #python312Packages.loguru
-            #python312Packages.requests
-            #python312Packages.vulture
-            uv
-            ruff
-            grpc-tools
-            gcc
-            openssl_3
-            certstrap
-          ];
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python3
+              pkgs.uv
+            ];
 
-          shellHook = ''
+            env = lib.optionalAttrs pkgs.stdenv.isLinux {
+              # Python libraries often load native shared objects using dlopen(3).
+              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
 
-            if [ -z "$NIX_SHELL_NESTED" ]; then
-              export NIX_SHELL_NESTED=1
-              alias ipy="uv run ipython --nosep"
-              export PS1="🧢 \e[38;5;211m\]g\e[38;5;111mBLP\[\e[0m $PS1"
-              if [ ! -d .venv ]; then
-                echo "Running uv init, adding ipython and pip"
-                uv init .
-                uv add pip
-                uv add ipython
-                uv add grpcio
-                uv add grpcio-tools
+            shellHook = ''
+              unset PYTHONPATH
+              if [ -z "$NIX_SHELL_NESTED" ]; then
+                export NIX_SHELL_NESTED=1
+                alias ipy="uv run ipython --nosep"
+                export PS1="🧢 \e[38;5;211m\]g\e[38;5;111mBLP\[\e[0m $PS1"
+                if [ ! -d .venv ]; then
+                  echo "Running uv init, adding ipython and pip"
+                  uv init .
+                  uv add pip
+                  uv add ipython
+                  uv add grpcio
+                  uv add grpcio-tools
+                fi
+                uv run pip list
+              else
+                echo "Nested nix-shell detected, skipping uv init"
               fi
-              uv run pip list
-            else
-              echo "Nested nix-shell detected, skipping uv init"
-            fi
-
-          '';
-        };
-      });
+            '';
+          };
+        }
+      );
     };
 }
-
-
